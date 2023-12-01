f6f12f21c779f0b5ac5919873a6ad7f6053e7762
white <abhijithkkpurameri@gmail.com>
2023-12-01 21:08:43 +0530

    user dto created

---
+++ dtos/User.dto.ts
@@ -0,0 +1,21 @@
+import { IsEmail, IsEmpty, Length } from "class-validator";
+
+export class UserDto {
+  @IsEmpty({ message: "Name at least 3 more charactr" })
+  @Length(3, 225, { message: "Name at least 3 more charactr" })
+  fullName: string;
+
+  @IsEmpty({ message: "Enter Your mobile Number" })
+  @Length(10, 10, { message: "enter yout 10 digit number" })
+  mobNumber: number;
+
+  @IsEmail({}, { message: "Enter a proper Email Address" })
+  email: string;
+
+  @IsEmpty({ message: "Name at least 3 more charactr" })
+  @Length(3, 225, { message: "Name at least 3 more charactr" })
+  compName: string;
+
+  @IsEmpty({ message: "Choose afield" })
+  workShop: string;
+}
